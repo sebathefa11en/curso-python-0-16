ffe7aba8190ef0af43762ce4f7fa39ae09e6f7e4
Sebastian Gonzalez <sgonzalezc.dev@icloud.com>
2025-04-03 22:35:28 -0300

    ADD : Agrega carpeta y archivo dia-0
    Agrega carpeta y contenido del dia 0 del curso Python Total

---
+++ dia-0/Primer_Proyecto.py
@@ -0,0 +1,13 @@
+print("Hola Mundo, te digo 'Hola'")
+
+print("Hola " + 'Sebastian')
+
+print("Me llamo \"Sebastian\"")
+
+print("Esta es una linea\nY esta es otra linea")
+
+print("\t Esta es la tercera linea")
+
+print("This isn\'t a number")
+
+print("Este signo \\ es una barra invertida")
